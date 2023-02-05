a8fd2ca6c981252e8354b387ace2b9129f1f0d59
Arina <arinapostnikova123@gmail.com>
2023-02-05 17:06:13 +0300

    Initial commit

---
+++ APub 3.0/APub_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  APub_3_0App.swift
+//  APub 3.0
+//
+//  Created by Arina Postnikova on 5.02.23.
+//
+
+import SwiftUI
+
+@main
+struct APub_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
