97e3d478d776c0bfda4914a20c96fdbfa45511a7
Arina <arinapostnikova123@gmail.com>
2023-02-06 20:49:25 +0300

    fixies

--- APub 3.0/Views/BeerView.swift
+++ APub 3.0/Views/BeerView.swift
@@ -12,14 +12,23 @@ struct BeerView: View {
     
     var body: some View {
         HStack {
+            Spacer().frame(maxWidth: 20)
             Text(beer.country)
             Text(beer.name)
-            Text("\(beer.left)")
+            Text("\(String(format: "%.2f", beer.left))")
+            Spacer()
             Button {
                 
             } label: {
                 Image(systemName: "chevron.right")
             }
+            Spacer().frame(maxWidth: 10)
         }
+    }
+}
+
+struct BeerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeerView(beer: Beer(country: "🇲🇽", name: "Corona", price: 1.4, left: 100))
     }
 }
--- APub 3.0/Views/ContentView.swift
+++ APub 3.0/Views/ContentView.swift
@@ -8,39 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var beers = [Beer]()
+    @State var beers: [Beer] = [
+        Beer(country: "🇲🇽", name: "Corona", price: 1.4, left: 100),
+        Beer(country: "🇺🇸", name: "Budweiser", price: 0.8, left: 89),
+        Beer(country: "🇳🇱", name: "Heineken", price: 1, left: 68)
+    ]
     
     var revenuePerDay: Double = 0.0
     var soldBeer: Double = 0.0
     
-    func setupBeers() {
-        beers.append(Beer(country: "🇲🇽", name: "Corona", price: 1.4, left: 100))
-        beers.append(Beer(country: "🇺🇸", name: "Budweiser", price: 0.8, left: 89))
-        beers.append(Beer(country: "🇳🇱", name: "Heineken", price: 1, left: 68))
-    }
-    
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Revenue per day: \(revenuePerDay) $")
-                    .font(.system(size: 20))
-                
-                Text("Sold beer: \(soldBeer) L")
-                    .font(.system(size: 20))
-                Button {
+            HStack {
+                Spacer().frame(maxWidth: 20)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Revenue per day: \(String(format: "%.2f", revenuePerDay)) $")
+                        .font(.system(size: 20))
                     
-                } label: {
-                    Text("NEW SHIFT")
-                        .frame(width: 320)
-                }
-                .buttonStyle(.bordered)
-            }
-            .padding(.top, 10)
-            VStack {
-                List {
-                    ForEach(beers) { beer in
-                        BeerView(beer: beer)
+                    Text("Sold beer: \(String(format: "%.2f", soldBeer)) L")
+                        .font(.system(size: 20))
+                    Button {
+                        
+                    } label: {
+                        Text("NEW SHIFT")
+                            .frame(
+                                minWidth: 0,
+                                maxWidth: .infinity,
+                                alignment: .center
+                            )
                     }
+                    .buttonStyle(.bordered)
+                }
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    alignment: .topLeading
+                )
+                .padding(.top, 10)
+                Spacer().frame(maxWidth: 20)
+            }
+            List {
+                ForEach(beers) { beer in
+                    BeerView(beer: beer)
                 }
             }
         }
